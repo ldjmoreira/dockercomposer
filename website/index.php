1b7a3bcf41d09cc8740112c7b8a218f02509611c
ldjmoreira <ldjmoreira@hotmail.com>
2023-11-05 21:31:18 -0300

    initial project in dockercomposer

---
+++ website/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP Test</title>
+</head>
+<body>
+    <h1>PHP Test Page</h1>
+    <p>This is a test PHP page running on your Docker container with Apache and PHP.</p>
+    <?php
+    echo "PHP is working!";
+    ?>
+</body>
+</html>
